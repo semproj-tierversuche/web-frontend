ad6130013ae3e10843afda9635a7ac2f6532e33c
Alpay Yilmaz <alpay.yilmaz95@gmail.com>
2017-11-21 23:04:15 +0100

    Added navigation bar

---
+++ src/app/material/material.angular.ts
@@ -0,0 +1,8 @@
+import {MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule} from '@angular/material';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule],
+})
+export class MaterialAngularModule { }
