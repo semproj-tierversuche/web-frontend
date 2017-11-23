97a1282b7878b2e487eb471a85ccd91c0b58bbe1
Alpay Yilmaz <alpay.yilmaz95@gmail.com>
2017-11-23 19:06:22 +0100

    Installed Material Angular

--- src/app/material/material.angular.ts
+++ src/app/material/material.angular.ts
@@ -1,8 +1,8 @@
-import {MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule, MatTableModule} from '@angular/material';
 import {NgModule} from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule, MatTableModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatButtonToggleModule, MatTabsModule, MatTableModule],
 })
 export class MaterialAngularModule { }
